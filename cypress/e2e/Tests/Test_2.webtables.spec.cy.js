1fce4fe28baba7859b110df71e38d66b0408e8e6
IIzmaylova <irina.shad.93@yandex.ru>
2023-03-21 12:55:20 +0500

    Added new tests

--- cypress/e2e/Tests/Test_2.webtables.spec.cy.js
+++ cypress/e2e/Tests/Test_2.webtables.spec.cy.js
@@ -1,5 +1,4 @@
-import { DATA} from '../../support/Page/webtalesLocators';
-import { WEBTABLES} from '../../support/Page/webtalesLocators';
+import { WEBTABLES, DATA} from '../../support/Page/webtalesLocators';
 import Table from '../../support/Page/table'
 
 
@@ -47,9 +46,30 @@ describe('Webtable tests', () => {
       wt.webtableIsOpen() // таблица открыта
       wt.getAllRecords().should('have.length', 3) // число записей не изменилось
     })
+
+    // Дополнительный тест.
+    it('4. При частично заполненных полях и нажатии на Submit форма остается открытой, запись не создается', () => {
+
+      wt.clickButtonAdd()  //При нажатии на кнопку Add
+      wt.getModalWindow().should('be.exist') // откроется форма создания записи
+      
+      wt.checkFormIsEmpty() // проверяем, что форма пустая
+
+      wt.fillFieldFirstname() //заполняем одно поле
+
+      wt.clickButtonSubmit()  //При нажатии на кнопку Submit
+      wt.getModalWindow().should('be.exist') // форма остается открытой
+
+        // Новая запись не должна появиться, проверяем осталось ли количество записей прежним
+      wt.webtableIsOpen() // таблица открыта
+      wt.getAllRecords().should('have.length', 3) // число записей не изменилось
+
+      // проверяем, что в таблице отсутствует запись с данными из частично заполненной формы
+      wt.getAllRecords().should('not.contain', DATA.firstName) 
+    })
     
 
-    it('4. При заполненных полях форма закрывается, появляется запись в таблице', () => {
+    it('5. При заполненных полях форма закрывается, появляется запись в таблице', () => {
 
         wt.clickButtonAdd()  //При нажатии на кнопку Add
         wt.getModalWindow().should('be.exist') // откроется форма создания записи
@@ -66,33 +86,52 @@ describe('Webtable tests', () => {
 
     })
 
+// Дополнительный тест. Проверяет, что при попытке добавить дублирующуюся запись форма не закрывается,
+// появляется сообщение "The user already exists"
+    it('6. При попытке добавить дублирующуюся запись появляется сообщение "The user already exists"', () => {
 
+      wt.clickButtonAdd()  //При нажатии на кнопку Add
+      wt.getModalWindow().should('be.exist') // откроется форма создания записи
+      
+      wt.makeDoubleRecNum(1)   // дублируем первую запись
+      
+      wt.clickButtonSubmit()  //При нажатии на кнопку Submit
+      wt.getModalWindow().should('be.exist') // форма не закрылась
+      
+      // Проверяем наличие записи о том, что такой пользоатель уже есть
+      cy.contains('The user already exists').should('be.exist') 
 
-  it('5. При удалении созданной записи - она и только она пропадает из таблицы', () => {
+      wt.getAllRecords().should('have.length', 3) // Количество записей не изменилось
 
-    wt.webtableIsOpen() //таблица открыта
-
-    wt.deleteRecordNum(1) // Удаляем первую запись
-    
-    // Проверяем, что удаляемая запись исчезла из таблицы, а остальные остались
-    wt.getAllRecords()
-      .should('contain', Records[1])
-      .and('contain', Records[2])
-      .and('not.contain', Records[0])
   })
 
 
-  it('6. При поиске по имени Cierra остается только одна строчка с данными по Cierra', () => {
 
-    wt.webtableIsOpen()  //открывается таблица//
+    it('7. При удалении созданной записи - она и только она пропадает из таблицы', () => {
 
-    wt.searchValue("Cierra") // В окно поиска вводим значение "Cierra"
+      wt.webtableIsOpen() //таблица открыта
 
-    // Проверяем, что в выдаче результатов только одна запись, удолетворяющая условию поиска
-    wt.getAllRecords()
-      .should('have.length', 1)
-      .should('contain', 'Cierra')
-  })
+      wt.deleteRecordNum(1) // Удаляем первую запись
+      
+      // Проверяем, что удаляемая запись исчезла из таблицы, а остальные остались
+      wt.getAllRecords()
+        .should('contain', Records[1])
+        .and('contain', Records[2])
+        .and('not.contain', Records[0])
+    })
+
+
+    it('8. При поиске по имени Cierra остается только одна строчка с данными по Cierra', () => {
+
+      wt.webtableIsOpen()  //открывается таблица//
+
+      wt.searchValue("Cierra") // В окно поиска вводим значение "Cierra"
+
+      // Проверяем, что в выдаче результатов только одна запись, удолетворяющая условию поиска
+      wt.getAllRecords()
+        .should('have.length', 1)
+        .should('contain', 'Cierra')
+    })
 
   
 })
